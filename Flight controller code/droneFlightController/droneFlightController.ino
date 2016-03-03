c49155316bb744c62e2036866ea3b361add06c86
Camiel <camielk@live.nl>
2016-03-03 21:12:42 +0100

    added mpu6050 gyroscope to project
    mpu6050 FIFO buffer is overflowing due to only reading 60 times per
    second. ignored for now, but produces odd values every now and then

--- Flight controller code/droneFlightController/droneFlightController.ino
+++ Flight controller code/droneFlightController/droneFlightController.ino
@@ -1,7 +1,7 @@
 /****************************************************************************************
  *  
- *  Version: 0.1
- *  Date: 18-2-2016
+ *  Version: 0.2
+ *  Date: 03-3-2016
  *  Author: Camiel Kerkhofs
  *  
  *  Description: This arduino script serves as flight controller for a custom made quadcopter drone
@@ -19,46 +19,142 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h> //used for compass module
 #include <Adafruit_HMC5883_U.h> //used for compass module
-
+#include "I2Cdev.h" //used for communication with MPU6050
+#include "MPU6050_6Axis_MotionApps20.h" //used for calculating MPU6050 values
 
 //defines
 #define trigPin 13
 #define echoPin 12
+bool blinkState = false;
 
 //assign unique ID to compass sensor
 Adafruit_HMC5883_Unified compass = Adafruit_HMC5883_Unified(12345);
+
+//initiate mpu at default adress 0x68 (use 'MPU6050 mpu(0x69);' for alternative adress)
+MPU6050 mpu;
 
 //timing variables
 unsigned long lastRunTime = micros();
 int logicUpdatesPerSecond = 60;
 int updateInterval = 1000000 / logicUpdatesPerSecond;
+int loopCount = 0;
 
 //sensor variables
 float heading; //heading in degrees from magnetic north
 float elevation = 0; //height
+float yaw;
+float pitch;
+float roll;
 
-int loopCount = 0;
+// MPU control/status variables
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion variables
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+//interrupt detection
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
 
 
 void setup() {
-  Serial.begin(115200); Serial.println(""); Serial.println("Flight Controller entered setup.");
+    // initialize serial communication
+    Serial.begin(115200); Serial.println(""); Serial.println("Flight Controller entered setup.");
 
-  //initialise compass sensor
-  if(!compass.begin()) {
-    Serial.println(">>>ERROR<<< unable to connect with HMC5883");
-    while(1);
-  }
-  sensor_t sensor;
-  compass.getSensor(&sensor);
 
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing mpu device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+    
+    //initialize compass sensor
+    Serial.println(F("Initializing compass..."));
+    if(!compass.begin()) {
+      Serial.println(">>>ERROR<<< unable to connect with HMC5883");
+      while(1);
+    }
+    sensor_t sensor;
+    compass.getSensor(&sensor);
+
+    //set custom mpu6050 offsets
+    mpu.setXGyroOffset(-18);
+    mpu.setYGyroOffset(-40);
+    mpu.setZGyroOffset(16);
+    mpu.setXAccelOffset(-2880);
+    mpu.setYAccelOffset(-110);
+    mpu.setZAccelOffset(1565);
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+
+    //configure in and outputs
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
 
 
 
 void loop() {
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
   Serial.println("Flight Controller is executing.");
+
+  //timing loop
   int ticks = 0;
   long lastTimer = millis();
   while (true) {
@@ -80,11 +176,11 @@ void loop() {
 
 void tick() { //main function to update sensors and adjust flightplan
 
-  loopCount++;
-  if (loopCount == 10) {
-    loopCount = 0;
+  //loopCount++;
+  //if (loopCount == 10) {
+  //  loopCount = 0;
     updateSensorData();
-  }
+  //}
 
   updateFlightPlan();
 }
@@ -94,12 +190,16 @@ void tick() { //main function to update sensors and adjust flightplan
 void updateSensorData() { //update sensor information
   heading = getCompassReading();
   elevation = getSonarReading();
+  getMpuReading(); //get gyro reading
 }
 
 
 void updateFlightPlan() { //calculate adjustments according to sensor data
   
 }
+
+
+
 
 float getCompassReading() {
   //create new event
@@ -170,9 +270,46 @@ float getSonarReading() {
 }
 
 
+void getMpuReading() {
+    //get INT_STATUS byte
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        //Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+        
+        // display Euler angles in degrees
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetGravity(&gravity, &q);
+        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+        yaw = ypr[0] * 180/M_PI;
+        pitch = ypr[1] * 180/M_PI;
+        roll = ypr[2] * 180/M_PI;
+    }
+}
+
+
+
 //send log message over serial
 void sendLogMsg() {
-  String logMsg = String("|h" + String(heading) + "|e" + String(elevation) + "|");
+  String logMsg = String("|h" + String(heading) + "|e" + String(elevation) + "|y" + String(yaw) + "|p" + String(pitch) + "|r" + String(roll) + "|");
   Serial.println(logMsg);
 }
 
